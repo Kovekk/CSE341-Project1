c379689ff28991b930164bcb1c4cb379eea2d53f
Kovekk <rbrtlwry@gmail.com>
2024-06-20 11:59:16 -0600

    attempt 2 to fix swwagger docs issue

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger-output.json');
 
-router.get('/', (req, res) => {res.send('Hello World')});
+router.use('/', swaggerUi.serve);
+router.get('/', swaggerUi.setup(swaggerDocument)); 
 
 router.use('/contacts', require('./contacts'));
 
-router.use('/api-docs', swaggerUi.serve);
-router.get('/api-docs', swaggerUi.setup(swaggerDocument)); 
+
 
 module.exports = router;
